e1a3486da31d6945f9a339a03c18c3d11bb67214
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-03-19 22:22:13 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -21,7 +21,7 @@ class Var(object):
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001871766752'))
     NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1001821439025'))
     PORT = int(getenv('PORT', '8080'))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'starmovies.onrender.com'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "1391556668").split()]
     NO_PORT = bool(getenv('NO_PORT', False))
